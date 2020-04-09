e19dbbaa9288e5898422b005c4e2d339ea561077
Buldomas <laurent.soubigou445@orange.fr>
2020-04-09 07:29:34 +0200

    Finalisation complete

---
+++ present.js
@@ -0,0 +1,25 @@
+
+
+var monbouton = document.getElementById("btn");
+monbouton.addEventListener("click", function () {
+  basculeMenu();
+});
+
+var monsand = document.getElementById("menu1");
+monsand.addEventListener("click", function () {
+  basculeMenu();
+});
+
+function basculeMenu() {
+  var monmenuamoa = document.getElementById("menu1");
+  var monicone = document.getElementById("btnicone");
+
+  if (monmenuamoa.style.left != "0px") {
+    monmenuamoa.style.left = "0px";
+    monicone.style.left = "100%";
+  } else {
+    monmenuamoa.style.left = "100%";
+    monicone.style.left = "0px";
+  }
+  return;
+}
